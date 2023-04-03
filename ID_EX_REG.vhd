854c607e9d860394b890d2a88b3db97a8d3af032
Limin <xiongx659@gmail.com>
2023-04-02 22:32:46 -0400

    [bugfix](ID_EX) add input and output

--- ID_EX_REG.vhd
+++ ID_EX_REG.vhd
@@ -1,5 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity ID_EX_REG is
 	port(clk 							: in std_logic;
+	
+			 reg_dst_in 			: in std_logic; -- EX
 			 alu_op_in				: in std_logic_vector(2 downto 0); -- EX
 			 alu_src_in				: in std_logic; -- EX
 			 branch_in				: in std_logic; -- MEM
@@ -7,6 +12,14 @@ entity ID_EX_REG is
 			 mem_write_in			: in std_logic; -- MEM
 			 reg_write_in			: in std_logic; -- WB
 			 mem_to_reg_in		: in std_logic; -- WB
+			 pc_in						: in unsigned(31 downto 0);
+			 read_data1_in		: in unsigned(31 downto 0);
+			 read_data2_in		: in unsigned(31 downto 0);
+			 sign_extend_in	  : in std_logic_vector(31 downto 0));
+			 rt_addr_in				: in std_logic_vector(4 downto 0);
+			 rd_addr_in				: in std_logic_vector(4 downto 0);
+			 
+			 reg_dst_out 			: in std_logic; -- EX
 			 alu_op_out				: out std_logic_vector(2 downto 0); -- EX
 			 alu_src_out			: out std_logic; -- EX
 			 branch_out				: out std_logic; -- MEM
@@ -14,4 +27,11 @@ entity ID_EX_REG is
 			 mem_write_out		: out std_logic; -- MEM
 			 reg_write_out		: out std_logic; -- WB
 			 mem_to_reg_out		: out std_logic); -- WB
+			 pc_in						: out unsigned(31 downto 0);
+			 read_data1_in		: out unsigned(31 downto 0);
+			 read_data2_in		: out unsigned(31 downto 0);
+			 sign_extend_out	: out std_logic_vector(31 downto 0));
+			 rt_addr_in				: in std_logic_vector(4 downto 0);
+			 rd_addr_in				: out std_logic_vector(4 downto 0);
+			 
 end ID_EX_REG;
--- IF_ID_REG.vhd
+++ IF_ID_REG.vhd
@@ -8,3 +8,20 @@ entity IF_ID_REG is
 			 instruction_out: out std_logic_vector(31 downto 0));
 			 pc_out					: out unsigned(31 downto 0);
 end entity;
+
+architecture behavioral of IF_ID_REG is
+  signal pc_reg : unsigned(31 downto 0) := (others => '0');
+  signal instruction_reg : std_logic_vector(31 downto 0) := (others => '0');
+begin
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      pc_reg <= pc_in;
+      instruction_reg <= instruction_in;
+    end if;
+  end process;
+
+  pc_out <= pc_reg;
+  instruction_out <= instruction_reg;
+
+end architecture behavioral;
